5f069d5a2471c7cba4f7b2cf1127387dbe2bb5fc
plysnh <plysnh@gmail.com>
2024-08-25 02:28:50 -0600

    the other pages

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import React, { useState, useEffect } from "react";
+import { ChakraProvider } from '@chakra-ui/react'
+
+// import Searchbar from "./components/Searchbar/Searchbar";
+import Card from './components/Card/card.components';
+function App() {
+    let [fetchedData, updateFetchedData] = useState([]);
+    let { info, results } = fetchedData;
+
+    let api = `https://rickandmortyapi.com/api/character/?page=1`
+    //when api change, reload the new data
+    useEffect(() => {
+        (async function () {
+            let data = await fetch(api).then(res => res.json());
+            updateFetchedData(data)
+        }) ();
+    }, [api]);
+
+    return (
+        <ChakraProvider>
+            <div className="App">
+                <h1 className="text-center mb-3">Characters</h1>
+                <div className="container">
+                    <div className="row">
+                        Filter component will be placed here
+                        <div className="col-lg-8 col-12">
+                            <div className="row">
+                                <Card results={results}/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </ChakraProvider>
+
+    );
+}
+
+export default App;
